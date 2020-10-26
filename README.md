0f4d255c331d22d0d1705a4a189691bb121aaf9c
SpacingBat3 <57194920+SpacingBat3@users.noreply.github.com>
2020-10-26 20:13:46 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Discord Electron
+A Discord Web App based on the [Electron](https://github.com/electron/electron) engine.
+
+It is completly indpenendent from [Discord-Electron](https://github.com/GyozaGuy/Discord-Electron), but I used it to learn why my previous attepms of doing electron discord app have failed – it was because `nodeIntegration` was set to `true`. So thanks to the author for that comment he typed – wouldn't probably know that without checking it!
+
+I previously forked his work, but now "I've stolen what's mine" from the code I've written in the fork, polished it a little and done this project. I've rewritten it from scratch, so it's simpler than it were before (for me) ~~and actually removes the "variable hell" of the original project~~.
+
+## Run
+Go to your cloned repository and execute this as a regular user:
+```sh
+npm install && npm start
+```
+
+## Install
+Check the [releases](https://github.com/SpacingBat3/electron-discord-webapp/releases/).
+
+(Probably still empty through, so you need to run or build it by yourself)
+
+## Build
+I recommend building this app with the `electron-builder` tool.
+You can install it on Linux using `npm` with the following command:
+```sh
+sudo npm install -g electron-builder
+```
+See [their docs](https://www.electron.build/multi-platform-build) if you wish to learn how to use it.
+
+## License
+This project is redistributed under the [MIT License](LICENSE).
---
+++ main.js
@@ -0,0 +1,159 @@
+// Load the functions that we need to have from the electron
+const { app, BrowserWindow, Tray, Menu, MenuItem, shell } = require('electron')
+
+// Vars to modify app behavior
+var appName = 'Discord'
+var appURL = 'https://discord.com/app'
+var appIcon = 'icons/app.png'
+var appTrayIcon = 'icons/tray.png'
+var appTrayIconSmall = 'icons/tray-small.png'
+var wWidth = '1000'
+var wHeight = '600'
+
+// "About" information
+var appFullName = 'Electron Discord WebApp'
+var appVersion = '0.1.0'
+var appAuthor = 'Spacingbat3'
+var appCredits = "Thanks to GyozaGuy for his electron discord app – it was good source\nto learn about electron and making the Discord web app."
+var appYear = '2020' // the year from app exists
+var appRepo = "https://github.com/SpacingBat3/electron-discord-webapp"
+
+// Add yourself there if you're doing PR to this repository.
+// The valid format if JavaScript array. ( = [var,"string"] )
+// Removing any entry of array there will deny your PR!
+// Same when spamming random entries!
+
+var appContributors = [
+	appAuthor
+]
+
+// "Static" Variables that shouldn't be changed
+
+var chromeVersion = process.versions['chrome']
+let tray = null
+var wantQuit = false
+var currentYear = new Date().getFullYear()
+var stringContributors = appContributors.join(', ')
+
+// Year format for copyright
+if (appYear == currentYear){
+	var copyYear = appYear
+} else {
+	var copyYear = `${appYear}-${currentYear}`
+}
+
+// Checks the platform and generates the proper User Agent:
+if (process.platform == 'darwin') {
+	var fakeUserAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${chromeVersion} Safari/537.36`;
+} else if (process.platform == 'win32') {
+	var fakeUserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${chromeVersion} Safari/537.36`;
+} else {
+	var fakeUserAgent = `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${chromeVersion} Safari/537.36`;
+};
+
+function createWindow () {
+
+	//Browser window
+	
+	const win = new BrowserWindow({
+		tittle: appName,
+		width: wWidth,
+		height: wHeight,
+		backgroundColor: "#202225",
+		icon: appIcon,
+		webPreferences: {
+			nodeIntegration: false, // won't work with the true value
+			devTools: false
+		}
+	})
+	win.loadURL(appURL,{userAgent: fakeUserAgent})
+	win.setAutoHideMenuBar(true);
+	win.setMenuBarVisibility(false);
+
+	// Contex Menu with spell checker
+
+	win.webContents.on('context-menu', (event, params) => {
+		const cmenu = new Menu()
+		for (const suggestion of params.dictionarySuggestions) {
+			cmenu.append(new MenuItem({
+				label: suggestion,
+				click: () => win.webContents.replaceMisspelling(suggestion)
+			}))
+		}
+		if (params.misspelledWord) {
+			cmenu.append(
+				new MenuItem({
+					label: 'Add to dictionary',
+					click: () => win.webContents.session.addWordToSpellCheckerDictionary(params.misspelledWord)
+				})
+			)
+		}
+		cmenu.popup()
+	})
+
+	// Tray menu
+
+	tray = new Tray(appTrayIcon)
+	const contextMenu = Menu.buildFromTemplate([
+		{ label: 'Top Secret Cotrol Panel', enabled: false, icon: appTrayIconSmall },
+		{ type: 'separator' },
+		{ label: 'About', role: 'about', click: function() { app.showAboutPanel();;}},
+		{ type: 'separator' },
+		{ label: 'Toogle', click: function() { win.isVisible() ? win.hide() : win.show();; } },
+		{ label: 'Quit Discord', click: function() {  wantQuit=true; app.quit();; } }
+	])
+	tray.setToolTip('Discord')
+	tray.setContextMenu(contextMenu)
+
+	// Exit to tray
+
+	win.on('close', (event) => {
+		if (!wantQuit){
+			event.preventDefault()
+			win.hide()
+		}
+	})
+
+	// Open external URLs in default browser
+	win.webContents.on('new-window', (event, externalURL) => {
+		event.preventDefault();
+		shell.openExternal(externalURL)
+	})
+}
+
+const singleInstance = app.requestSingleInstanceLock()
+
+if (!singleInstance) {
+	app.quit()
+} else {
+	app.on('second-instance', (event, commandLine, workingDirectory) => {
+		if (win && win.isMinimized()) win.restore()
+		win.focus()
+	})
+	app.on('ready', () => {
+		createWindow()
+	})
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow()
+	}
+})
+
+// About panel
+app.setAboutPanelOptions({
+	applicationName: appFullName,
+	iconPath: appIcon,
+	applicationVersion: appVersion,
+	authors: appContributors,
+	website: appRepo,
+	credits: `Authors and contributors: ${stringContributors}`,
+	copyright: `Copyright © ${copyYear} ${appAuthor}\n\n${appCredits}`
+})
