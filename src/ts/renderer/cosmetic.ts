c0fe2e9a6400b7fb6fef88423a75e3afff22ed5f
SpacingBat3 <git@spacingbat3.anonaddy.com>
2021-06-20 17:42:25 +0200

    "Isolate" `cosmetic.ts` functions.
    Because 'contextIsolation' is currently disabled, other scripts could gain
    an access to `removeUnneded()` function to use it to
    eg. remove servers from the side bar. This shouldn't be now the case.

--- src/ts/renderer/cosmetic.ts
+++ src/ts/renderer/cosmetic.ts
@@ -3,22 +3,23 @@
  */
 
 import { wLog } from '../global';
+{
+  const removeUnneded = () => {
+    // If user is at login/register website, do not apply any cosmetic changes
+    if (document.URL.includes('login')||document.URL.includes('register')) return;
 
-function removeUnneded() {
-  // If user is at login/register website, do not apply any cosmetic changes
-  if (document.URL.includes('login')||document.URL.includes('register')) return;
+    const orangePopup = document.querySelector('.notice-3bPHh-');
+    const sideBarList = document.querySelectorAll('.listItem-GuPuDH');
 
-  const orangePopup = document.querySelector('.notice-3bPHh-');
-  const sideBarList = document.querySelectorAll('.listItem-GuPuDH');
-
-  if (orangePopup!==null) orangePopup.remove(); // Remove popup that appears when using Discord's web version
-  if (sideBarList.length!==0) {
-    sideBarList[sideBarList.length-1].remove(); // Remove "app download" button
-    sideBarList[sideBarList.length-2].remove(); // Remove separator
-    wLog("Successfully removed unnecesarry elements on website.");
-  } else {
-    wLog("Website hasn't been fully loaded yet, retrying after 0.5s...");
-    setTimeout(removeUnneded, 500);
+    if (orangePopup!==null) orangePopup.remove(); // Remove popup that appears when using Discord's web version
+    if (sideBarList.length!==0) {
+      sideBarList[sideBarList.length-1].remove(); // Remove "app download" button
+      sideBarList[sideBarList.length-2].remove(); // Remove separator
+      wLog("Successfully removed unnecesarry elements on website.");
+    } else {
+      wLog("Website hasn't been fully loaded yet, retrying after 0.5s...");
+      setTimeout(removeUnneded, 500);
+    }
   }
+  window.onload = removeUnneded;
 }
-window.onload = removeUnneded;
