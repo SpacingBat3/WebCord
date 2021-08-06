88262a7062035a5d1644fd8bd6466957d869402e
SpacingBat3 <git@spacingbat3.anonaddy.com>
2021-08-06 14:11:09 +0200

    Improve language implementation.
    See updated translation documentation (`Translate.md` file) for long
    describtion about the recent changes done.

--- docs/Translate.md
+++ docs/Translate.md
@@ -1,31 +1,59 @@
-## How to translate?
+## WebCord's localization notes and tips
 
-- fork this project,
+This document will describe how to properly tranlate WebCord client to your language (if you feel more comfortable in using it in your native language rather than in English).
 
-- clone (download) it to your computer,
+### Recent changes
 
-- create new folder for your translation (check
-  [electron docs](https://www.electronjs.org/docs/api/locales) for list of
-  possible codes – names of the folders),
+As of WebCord release 1.6.0 or newer, the way how unofficial translations are handled has been changed – because many translation files were broken after recent updates in the past (mostly Spanish ones), I decided to **not to include any of the community-made translations** and let others host them on their own rules/licenses in their own repositories. It will now support an additional directory, which is `/path/to/app.asar/../translations`. The folder is basically treaded by WebCord as `sources/assets/translations` internal application folder, except it has a lower priority and strings from it are loaded only in case of missing localized translations (so no malicious changes can be made to ).
 
-- copy the lang strings you want to translate from (mostly
-  [soures/assets/en-GB/strings.jsonc](../sources/assets/en-GB/strings.jsonc)) to
-  the dir with code name of your language you want to translate to,
+If you want to recover the removed translations, you can easily revert back recent commits or fetch them from the older releases source code. If you wish to share somewhere your translations, you are free to make a new thread in General category at WebCord's Github Discussions page. As of Linux distributions, I also advise to distribute translations in the format that is friendly to package (e.g. by distributing a `PKGBUILD` and/or publishing it to AUR) – please remember then to add a proper WebCord package as a dependency.
 
-- translate strings, using any application for translating JSON files or any
-  text editor (if you know the JSON syntax).
+In the future, I may create a deticated repository for community translations only, if someone wants to host them in one place.
 
-- publish the strings to your GitHub fork,
+### How to translate?
 
-- do a Pull Request (here's
-  [random youtube tutorial](https://www.youtube.com/watch?v=dSl_qnWO104)
-  how to do that),
+#### How WebCord implements translations
 
-- you're done! You're safe now to remove your WebCord fork if you want to.
+Before you will begin to translate WebCord, you should understood a basics about WebCord's localization support implementation.
 
-## The people that helped me with the app translation:
-(add yourself there if you weren't added before/after you do a PR):
+In the WebCord sources, you may have noticed the following directory: `sources/assets/translations`. This is the folder where official translations are published and which are always distributed within the application itself.
 
-- [MrCoolAndroid](https://github.com/MrCoolAndroid) – Spanish translation
+To get information about the system language, WebCord also uses [`app.getLocale()`](https://www.electronjs.org/docs/api/app#appgetlocale) Electron API, which (as described in Electron Docs) uses the Chromium `l10n_util` library to get information about the system language. Hence the folder names in `translations` folder are possible values returned by `app.getLocale()` function, the list of possible code names is described [here](https://source.chromium.org/chromium/chromium/src/+/master:ui/base/l10n/l10n_util.cc;l=56-232).
 
-- [ElWazy](https://github.com/ElWazy) – Spanish translation
+#### Supported file formats/extensions
+
+As of the translation format, WebCord is capable of understanding the two file formats:
+
+- Regular JSON files.
+- JSONC (aka. *JSON with Comments*) file format.
+
+JSONC parser supports following comment styles:
+
+- C-like end-of-line comments:
+```jsonc
+// Example of end-of-line comment
+```
+
+- C++-like block comments (both single-line and multi-line):
+```ts
+/* Single-line comment */
+
+/**
+ * Multi-line comment
+ * 
+ * Can be used for longer description 
+ */
+
+/*
+ * Two 'star' symbols for opening a comment
+ * isn't a requirement BTW
+ */
+```
+
+#### Translating WebCord and testing translations
+
+- Get reference strings from the repository (for English strings: [soures/assets/en-GB/strings.jsonc](../sources/assets/en-GB/strings.jsonc)).
+- Check which is the code name of the language to which you want to translate WebCord, that is supported by Chromium L10N library. You find list of available language codes [in this file](https://source.chromium.org/chromium/chromium/src/+/master:ui/base/l10n/l10n_util.cc).
+- Create a new folder with the name of the target language – the code name you found in previous step.
+- Copy reference strings to that folder and translate them, either following [JSON standard](https://www.json.org) or JSONC file format if you want comments support. Be aware that comments in regular JSON files (`*.json`) are treated as a syntax error by WebCord.
+- If you're done with translating WebCord, you can test your translations by coping the folder that you previously made to `{Path where 'app.asar' is placed}/translations` – on Windows and Linux, `app.asar` is usually placed in the `resources` folder next to WebCord binary.
--- sources/code/lang.ts
+++ sources/code/lang.ts
@@ -8,38 +8,38 @@ import * as deepmerge from "deepmerge";
 import { app } from "electron";
 import { jsonParseWithComments } from "./global";
 
-function jsonOrJsonc(fileNoExtension:string):string {
-	if (fs.existsSync(fileNoExtension+'.jsonc'))
-		return fileNoExtension+'.jsonc'
+function jsonOrJsonc(fileNoExtension: string): string {
+	if (fs.existsSync(fileNoExtension + '.jsonc'))
+		return fileNoExtension + '.jsonc';
 	else
-		return fileNoExtension+'.json'
-} 
+		return fileNoExtension + '.json';
+}
 
 function isJsonTranslatedStrings(object: unknown): object is TranslatedStrings {
 	type TranslatedStringsKeys = {
+		topLevel: ['tray', 'context', 'menubar', 'dialog', 'help', 'misc', 'settings'];
+	};
+	const TranslatedStringsKeys: TranslatedStringsKeys = {
 		topLevel: ['tray', 'context', 'menubar', 'dialog', 'help', 'misc', 'settings']
-	}
-	const TranslatedStringsKeys:TranslatedStringsKeys = {
-		topLevel: ['tray', 'context', 'menubar', 'dialog', 'help', 'misc', 'settings']
-	}
+	};
 	let isTopObject = true;
-	for(const property of TranslatedStringsKeys.topLevel) if (typeof((object as TranslatedStrings)[property]) !== 'object')
+	for (const property of TranslatedStringsKeys.topLevel) if (typeof ((object as TranslatedStrings)[property]) !== 'object')
 		isTopObject = false;
-	return	(
+	return (
 		isTopObject &&
-		typeof((object as TranslatedStrings).menubar.file.options.csp) === 'object' &&
-		typeof((object as TranslatedStrings).menubar.file.addon.groupName) === 'string' &&
-		typeof((object as TranslatedStrings).menubar.view) === 'object' &&
-		typeof((object as TranslatedStrings).menubar.window) === 'string' &&
-		typeof((object as TranslatedStrings).dialog.buttons ) === 'object' &&
-		typeof((object as TranslatedStrings).dialog.mod) === 'object' &&
-		typeof((object as TranslatedStrings).dialog.permission.check) === 'object' &&
-		typeof((object as TranslatedStrings).dialog.permission.request) === 'object' &&
-		typeof((object as TranslatedStrings).dialog.ver) === 'object' &&
-		typeof((object as TranslatedStrings).settings.basic.group.menuBar) === 'object' &&
-		typeof((object as TranslatedStrings).settings.basic.group.tray) === 'object' &&
-		typeof((object as TranslatedStrings).settings.advanced.group.csp.group.thirdparty.list.gifProviders) === 'string'
-	)
+		typeof ((object as TranslatedStrings).menubar.file.options.csp) === 'object' &&
+		typeof ((object as TranslatedStrings).menubar.file.addon.groupName) === 'string' &&
+		typeof ((object as TranslatedStrings).menubar.view) === 'object' &&
+		typeof ((object as TranslatedStrings).menubar.window) === 'string' &&
+		typeof ((object as TranslatedStrings).dialog.buttons) === 'object' &&
+		typeof ((object as TranslatedStrings).dialog.mod) === 'object' &&
+		typeof ((object as TranslatedStrings).dialog.permission.check) === 'object' &&
+		typeof ((object as TranslatedStrings).dialog.permission.request) === 'object' &&
+		typeof ((object as TranslatedStrings).dialog.ver) === 'object' &&
+		typeof ((object as TranslatedStrings).settings.basic.group.menuBar) === 'object' &&
+		typeof ((object as TranslatedStrings).settings.basic.group.tray) === 'object' &&
+		typeof ((object as TranslatedStrings).settings.advanced.group.csp.group.thirdparty.list.gifProviders) === 'string'
+	);
 }
 
 /**
@@ -56,170 +56,175 @@ function isJsonTranslatedStrings(object: unknown): object is TranslatedStrings {
  */
 
 export class TranslatedStrings {
-	readonly tray!: {
-		readonly toggle: string;
-		readonly quit: string;
+	tray: {
+		toggle: string;
+		quit: string;
 	};
-	readonly context!:{
-		readonly copy: string;
-		readonly paste: string;
-		readonly cut: string;
-		readonly dictionaryAdd: string;
-		readonly copyURL: string;
-		readonly copyURLText: string;
-		readonly inspectElement: string;
+	context: {
+		copy: string;
+		paste: string;
+		cut: string;
+		dictionaryAdd: string;
+		copyURL: string;
+		copyURLText: string;
+		inspectElement: string;
 	};
-	readonly menubar!: {
-		readonly enabled: string;
-		readonly file: {
-			readonly groupName: string;
-			readonly quit: string;
-			readonly relaunch: string;
-			readonly options: {
-				readonly groupName: string;
-				readonly disableTray: string;
-				readonly hideMenuBar: string;
-				readonly mobileMode: string;
-				readonly csp: {
-					readonly groupName: string;
-					readonly thirdparty: string;
-					readonly gifProviders: string;
+	menubar: {
+		enabled: string;
+		file: {
+			groupName: string;
+			quit: string;
+			relaunch: string;
+			options: {
+				groupName: string;
+				disableTray: string;
+				hideMenuBar: string;
+				mobileMode: string;
+				csp: {
+					groupName: string;
+					thirdparty: string;
+					gifProviders: string;
 				};
-				readonly develMode: string;
+				develMode: string;
 			};
-			readonly addon: {
-				readonly groupName: string;
-				readonly loadNode: string;
-				readonly loadChrome: string;
-			}
-		};
-		readonly edit: string;
-		readonly view: {
-			readonly groupName: string;
-			readonly reload: string;
-			readonly forceReload: string;
-			readonly devTools: string;
-			readonly resetZoom: string;
-			readonly zoomIn: string;
-			readonly zoomOut: string;
-			readonly fullScreen: string;
-		};
-		readonly window: string;
-	};
-	readonly dialog!: {
-		readonly error: string;
-		readonly warning: string;
-		readonly ver: {
-			readonly update: string;
-			readonly updateBadge: string;
-			readonly updateTitle: string;
-			readonly recent: string;
-			readonly newer: string;
-			readonly diff: string;
-		};
-		readonly permission: {
-			readonly request: {
-				readonly denied: string;
+			addon: {
+				groupName: string;
+				loadNode: string;
+				loadChrome: string;
 			};
-			readonly check: {
-				readonly denied: string;
-			}
 		};
-		readonly buttons: {
-			readonly continue: string;
-			readonly yes: string;
-			readonly no: string;
+		edit: string;
+		view: {
+			groupName: string;
+			reload: string;
+			forceReload: string;
+			devTools: string;
+			resetZoom: string;
+			zoomIn: string;
+			zoomOut: string;
+			fullScreen: string;
 		};
-		readonly mod: {
-			readonly nodeExt: string;
-			readonly crx: string;
-		};
-		readonly hideMenuBar: string;
-		readonly devel: string;
+		window: string;
 	};
-	readonly help!:{
-		readonly groupName: string;
-		readonly about: string;
-		readonly repo: string;
-		readonly docs: string;
-		readonly bugs: string;
-		readonly contributors: string;
-		readonly credits: string;
+	dialog: {
+		error: string;
+		warning: string;
+		ver: {
+			update: string;
+			updateBadge: string;
+			updateTitle: string;
+			recent: string;
+			newer: string;
+			diff: string;
+		};
+		permission: {
+			request: {
+				denied: string;
+			};
+			check: {
+				denied: string;
+			};
+		};
+		buttons: {
+			continue: string;
+			yes: string;
+			no: string;
+		};
+		mod: {
+			nodeExt: string;
+			crx: string;
+		};
+		hideMenuBar: string;
+		devel: string;
 	};
-	readonly settings!: {
-		readonly basic: {
-			readonly name: string;
-			readonly group: {
-				readonly menuBar: {
-					readonly name: string;
-					readonly description: string;
-					readonly label: string;
+	help: {
+		groupName: string;
+		about: string;
+		repo: string;
+		docs: string;
+		bugs: string;
+		contributors: string;
+		credits: string;
+	};
+	settings: {
+		basic: {
+			name: string;
+			group: {
+				menuBar: {
+					name: string;
+					description: string;
+					label: string;
 				};
-				readonly tray: {
-					readonly name: string;
-					readonly description: string;
-					readonly label: string;
-				}
-			}
+				tray: {
+					name: string;
+					description: string;
+					label: string;
+				};
+			};
 		};
-		readonly advanced: {
-			readonly name: string;
-			readonly group: {
-				readonly csp: {
-					readonly name: string;
-					readonly description: string;
-					readonly group: {
-						readonly thirdparty: {
-							readonly name: string,
-							readonly list: {
-								readonly gifProviders: string;
-							}
-						}
-					}
-				}
-			}
-		}
+		advanced: {
+			name: string;
+			group: {
+				csp: {
+					name: string;
+					description: string;
+					group: {
+						thirdparty: {
+							name: string,
+							list: {
+								gifProviders: string;
+							};
+						};
+					};
+				};
+			};
+		};
 	};
-	readonly misc!: {
-		readonly singleInstance: string;
-	}
-	constructor () {
-		let l10nStrings:Record<string,unknown>;
+	misc: {
+		singleInstance: string;
+	};
+	constructor() {
+		let l10nStrings: Record<string, unknown>;
 		/**
 		 * Translated strings in the user TranslatedStringsuage.
 		 * 
 		 * @todo
 		 * Make `localStrings` not overwrite `l10nStrings`
-		 * when they are of wrong type.
+		 * when it is of wrong type.
 		 */
-		let localStrings:Record<string,unknown>;
-		const systemTranslatedStrings:string = app.getLocale();
-	
-		const localizedStrings = path.resolve(app.getAppPath(),"./sources/assets/translations/"+systemTranslatedStrings+"/strings")
-		const fallbackStrings = path.resolve(app.getAppPath(),"./sources/assets/translations/en-GB/strings")
-		
-		if(!app.isReady()) console.warn(
-			"[WARN] Electron may fail loading localized strings,\n"+
-			"       because the app hasn't still emitted the 'ready' event!\n"+
+		let localStrings: Record<string, unknown>;
+
+		const fallbackStrings = path.resolve(app.getAppPath(), "sources/assets/translations/en-GB/strings");
+		let localizedStrings = path.resolve(app.getAppPath(), "sources/assets/translations/" + app.getLocale() + "/strings");
+		const externalStrings = path.resolve(path.dirname(app.getAppPath()), 'translations/' + app.getLocale() + "/strings");
+
+		/* Handle unofficial translations */
+
+		if (!fs.existsSync(jsonOrJsonc(localizedStrings)))
+			localizedStrings = externalStrings;
+
+		if (!app.isReady()) console.warn(
+			"[WARN] Electron may fail loading localized strings,\n" +
+			"       because the app hasn't still emitted the 'ready' event!\n" +
 			"[WARN] In this case, English strings will be used as a fallback.\n"
 		);
-	
-		l10nStrings = jsonParseWithComments({path: jsonOrJsonc(fallbackStrings)});
-		if(fs.existsSync(jsonOrJsonc(localizedStrings))) {
-			localStrings = jsonParseWithComments({path: jsonOrJsonc(localizedStrings)});
-			l10nStrings = deepmerge(l10nStrings, localStrings); 
+
+		l10nStrings = jsonParseWithComments({ path: jsonOrJsonc(fallbackStrings) });
+		if (fs.existsSync(jsonOrJsonc(localizedStrings)) && localizedStrings !== fallbackStrings) {
+			localStrings = jsonParseWithComments({ path: jsonOrJsonc(localizedStrings) });
+			l10nStrings = deepmerge(l10nStrings, localStrings);
 		}
-		if(isJsonTranslatedStrings(l10nStrings)) {
-			this.tray = l10nStrings.tray
-			this.context = l10nStrings.context
-			this.dialog = l10nStrings.dialog
-			this.help = l10nStrings.help
-			this.menubar = l10nStrings.menubar
-			this.misc = l10nStrings.misc
-			this.settings = l10nStrings.settings
-			this.tray = l10nStrings.tray
+		if (isJsonTranslatedStrings(l10nStrings)) {
+			this.tray = l10nStrings.tray;
+			this.context = l10nStrings.context;
+			this.dialog = l10nStrings.dialog;
+			this.help = l10nStrings.help;
+			this.menubar = l10nStrings.menubar;
+			this.misc = l10nStrings.misc;
+			this.settings = l10nStrings.settings;
+			this.tray = l10nStrings.tray;
 		} else {
-			throw new TypeError('At least one of the JSON/JSONC strings translation files is of invalid type!')
+			throw new TypeError('At least one of the JSON/JSONC strings translation files is of invalid type!');
 		}
 	}
 }
