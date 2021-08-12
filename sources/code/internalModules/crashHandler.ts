711ba5361689080072598b96d66704fc633d6941
SpacingBat3 <git@spacingbat3.anonaddy.com>
2021-08-12 14:31:32 +0200

    Improve crashHandler output message.
    Do not log absolute path to TS source files and other crashHandler
    improvements.

--- sources/code/internalModules/crashHandler.ts
+++ sources/code/internalModules/crashHandler.ts
@@ -3,7 +3,7 @@ import * as colors from "colors/safe";
 
 /* Handles uncaughtException errors */
 
-export default function crashHandler():void {
+export default function crashHandler(): void {
     process.on('uncaughtException', async (error) => {
         let stack = "", message = "", stackColor = "";
         const name = "UncaughtException: " + app.getName() + " threw '" + error.name + "'.";
@@ -13,18 +13,27 @@ export default function crashHandler():void {
 
         if (error.stack !== undefined) {
             stack = "\n" + error.stack
-                .replace(RegExp(error.name + ': ' + error.message), '');
+                .replace(error.name + ': ' + error.message, '');
             stackColor = stack;
             const stackLines = stack.split(/\r\n|\n|\r/);
             const stackProcessed: string[] = [], stackColorProcessed: string[] = [];
-            for (const line of stackLines)
-                if (line.match(RegExp("at.*\\(" + app.getAppPath() + ".*\\).*", 'g'))) {
-                    stackProcessed.push(line);
-                    stackColorProcessed.push(line);
+            for (const line of stackLines) {
+                const regexAppPath = app.getAppPath().replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+                if (line.match(RegExp("at.*\\(" + regexAppPath + ".*\\).*", 'g'))) {
+                    let modifiedLine = line;
+                    const tsRule = line.match(/\(\/.*\.ts.*\)/);
+                    if (tsRule)
+                        modifiedLine = line.replace(tsRule[0].replace(new RegExp("\\((" + regexAppPath + "\\/).*\\)"), "$1"), "");
+                    stackProcessed.push(modifiedLine);
+                    stackColorProcessed.push(modifiedLine);
                 } else {
                     stackColorProcessed.push(colors.gray(line));
                 }
-            stack = stackProcessed.join('\n');
+            }
+            if (error.message !== "")
+                stack = "\n\n" + stackProcessed.join('\n');
+            else
+                stack = stackProcessed.join('\n');
             stackColor = stackColorProcessed.join('\n');
         }
 
@@ -32,7 +41,7 @@ export default function crashHandler():void {
         console.error('\n' + colors.red(colors.bold(name)) + colors.blue(message) + stackColor);
         dialog.showMessageBoxSync({
             title: name,
-            message: message + stack,
+            message: error.message + stack,
             type: 'error'
         });
 
