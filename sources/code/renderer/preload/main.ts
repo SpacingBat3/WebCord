2a94ffc342ee15f01f902750ecfcc80fe78e38e4
SpacingBat3 <git@spacingbat3.anonaddy.com>
2022-02-22 14:38:43 +0100

    Improve API key generation.
    I've decided to work again on the property generation to make it look
    less suspicious and harder to find – previously, there was a great
    chance to find it by property length, it's type and/or charset it uses.
    I've also add an additional check to make sure the generated key is not
    already the existing property name.

--- sources/code/renderer/preload/main.ts
+++ sources/code/renderer/preload/main.ts
@@ -1,16 +1,28 @@
 import { contextBridge, ipcRenderer } from "electron";
-import { randomBytes } from "crypto";
+import { randomInt } from "crypto";
 import { getAppIcon, wLog } from "../../global/global";
 import desktopCapturerPicker from "../modules/capturer";
 import preloadCosmetic from "../modules/cosmetic";
 import l10n from "../../global/modules/l10n";
 
 /**
+ * Generates a safe random key that is not present in the API.
+ */
+function generateSafeKey () {
+    const charset = 'abcdefghijklmnoprstuwxyzABCDEFGHIJKLMNOPRSTUWXYZ';
+    let key = '';
+    while(key === '' || key in window) {
+        key = '';
+        for(let i=0; i<=randomInt(4,32); i++)
+            key += charset.charAt(randomInt(charset.length-1));
+    }
+    return key;
+}
+/**
  * WebCord API key used as the object name of the exposed content
  * by the Context Bridge.
  */
-const contextBridgeApiKey = randomBytes(Math.floor((Math.random()+6)*32)).toString('base64');
-
+const contextBridgeApiKey = generateSafeKey();
 /*
  * Cosmetic script uses that to hide the Discord's download popup on fresh
  * installations.
