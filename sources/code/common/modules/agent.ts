c902db9777288d9d01862cd3f900c4570f88d4b9
SpacingBat3 <git@spacingbat3.anonaddy.com>
2022-07-15 13:52:00 +0200

    Add "mobile" user agent identifier.
    • Improve spoffing the user agent.
    • Add support for generating a "mobile" variant of user agents (it will
      include a "Mobile" keyword after "Chrome/", like it is with Android).
    
    Related to: #170 (GitHub issue).

--- sources/code/common/modules/agent.ts
+++ sources/code/common/modules/agent.ts
@@ -23,31 +23,34 @@ function getAgentArch() {
  * the user agent string (via `process.getSystemVersion()` in Electron API).
  * 
  * @param chromeVersion Chome/Chromium version string to use.
- * @param platform A Node.js platform to use instead of the OS ones.
+ * @param mobile Whenever user-agent should be for mobile devices.
+ * @param replace Genarate user-agent from provided `replace` data.
  * @returns Fake Chrome/Chromium user agent string.
  */
-export function getUserAgent(chromeVersion: string, platform?: NodeJS.Platform):string {
+export function getUserAgent(chromeVersion: string, mobile?: boolean, replace?: {platform: NodeJS.Platform, version: string, device?: string}):string {
 
-  const userAgentPlatfom = platform ?? process.platform;
+  const userAgentPlatfom = replace ? replace.platform : process.platform;
   let fakeUserAgent:string;
-  const osVersion = (typeof process.getSystemVersion === "function" ?
+  const osVersion = (replace ? replace.version : typeof process.getSystemVersion === "function" ?
     process.getSystemVersion() :
-    (userAgentPlatfom === "darwin" ?  "12.0" : os.release())
+    (userAgentPlatfom === "darwin" ? "12.0" : os.release())
   );
   let WOW64: string;
+  const device = (replace?.device ? "; "+replace.device : "");
+  const mobileAgent = mobile ? " Mobile" : "";
   switch (userAgentPlatfom) {
     case "darwin":
-      fakeUserAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X ${osVersion.replace(".","_")}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${chromeVersion} Safari/537.36`;
+      fakeUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X "+osVersion.replace(".","_")+") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"+chromeVersion+mobileAgent+" Safari/537.36";
       break;
     case "win32":
       osVersion.split(".")[0] === "10" ? WOW64 = "Win64; x64" : WOW64 = "WOW64";
-      fakeUserAgent = "Mozilla/5.0 (Windows NT "+osVersion+"; "+WOW64+") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"+chromeVersion+" Safari/537.36";
+      fakeUserAgent = "Mozilla/5.0 (Windows NT "+osVersion+"; "+WOW64+") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"+chromeVersion+mobileAgent+" Safari/537.36";
       break;
     case "android":
-      fakeUserAgent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"+chromeVersion+" Mobile Safari/537.36";
+      fakeUserAgent = "Mozilla/5.0 (Linux; Android "+osVersion+device+") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"+chromeVersion+mobileAgent+" Safari/537.36";
       break;
     default:
-      fakeUserAgent = "Mozilla/5.0 (X11; "+os.type()+" "+getAgentArch()+") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"+chromeVersion+" Safari/537.36";
+      fakeUserAgent = "Mozilla/5.0 (X11; "+os.type()+" "+getAgentArch()+") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"+chromeVersion+mobileAgent+" Safari/537.36";
   }
   return fakeUserAgent;
 }
