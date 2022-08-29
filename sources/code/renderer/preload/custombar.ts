6108d3638a4220e969403a92d3f8d41c8f084052
GooseSima <simaduck@gmail.com>
2022-08-29 11:35:22 +0300

    Optimization

--- sources/code/renderer/preload/custombar.ts
+++ sources/code/renderer/preload/custombar.ts
@@ -1,38 +1,18 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require("electron");
 
 window.addEventListener("DOMContentLoaded", () => {
-    /*
+  /*
     * Custom title bar
     */
 
-    const titlebar = document.createElement("div");
-    titlebar.className = "titlebar";
-
-    const title = document.createElement("div");
-    // title.innerText = location.host.split(".")[0]!;
-    title.innerText = "webcord";
-    title.className = "title";
-    const minimizebutton = document.createElement("div");
-    minimizebutton.className = "minimizebutton btn";
-    const maximizebutton = document.createElement("div");
-    maximizebutton.className = "maximizebutton btn";
-    const closebutton = document.createElement("div");
-    closebutton.className = "closebutton btn";
-    titlebar.appendChild(title);
-    titlebar.appendChild(minimizebutton);
-    titlebar.appendChild(maximizebutton);
-    titlebar.appendChild(closebutton);
-    document.body.prepend(titlebar);
-    title.onclick = function () {
-        ipcRenderer.send('opensettings', '')
-    };
-    minimizebutton.onclick = function () {
-        ipcRenderer.send('window-minimize', '')
-    };
-    maximizebutton.onclick = function () {
-        ipcRenderer.send('window-maximize', '')
-    };
-    closebutton.onclick = function () {
-        ipcRenderer.send('window-close', '')
-    };
+  const divs = document.body.getElementsByTagName('div');
+  for (let i = 0; i < divs.length; i++) {
+    const element = divs[i];
+    const action = element?.dataset['action'];
+    if (action){
+      element.onclick = function (){
+        ipcRenderer.send(action);
+      }
+    }
+  }
 });
