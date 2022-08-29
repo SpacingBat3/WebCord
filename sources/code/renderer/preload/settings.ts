b3fde3485aafed9fcf67121cd254821e40cb2a73
GooseSima <simaduck@gmail.com>
2022-08-29 10:19:41 +0300

    Update sources/code/renderer/preload/settings.ts
    Co-authored-by: SpacingBat3 <git@spacingbat3.anonaddy.com>

--- sources/code/renderer/preload/settings.ts
+++ sources/code/renderer/preload/settings.ts
@@ -91,7 +91,7 @@ function generateSettings(optionsGroups: htmlConfig) {
             });
           } else if(!("dropdown" in setting || "input" in setting || "keybind" in setting)) {
             (Object.keys as keys)(setting).sort().map(key => {
-              if (key !== "name" && key !== "description" && key !== "labels" && setting[key] !== undefined && setting.labels) {
+              if(key !== "name" && key !== "description" && key !== "labels" && setting[key] !== undefined) {
                 formContainer.appendChild(createForm({
                   type: "checkbox",
                   id: groupId + "." + settingKey + "." + key,
