1e896559d818cb369772ad2c414e40ebd2caec54
SpacingBat3 <git@spacingbat3.anonaddy.com>
2022-11-20 22:30:48 +0100

    Revert to old logic of `lftest`.
    Sometimes it's actually useful to commit `package.json` file only.

--- .husky/core.sh
+++ .husky/core.sh
@@ -80,7 +80,8 @@ function npkg() {
 function lftest () {
     mapfile -t FILES < <(git diff --staged --name-only);
     local no_meta no_lock;
-    unset -v no_lock no_meta;
+    no_lock=false;
+    no_meta=false;
     for file in "${FILES[@]}"; do
         if [[ "$file" == "package-lock.json" ]]; then
             no_lock=true;
@@ -89,10 +90,16 @@ function lftest () {
         fi
         [[ $no_lock && $no_meta ]] && break;
     done;
-    if [[ "$no_meta" != "$no_lock" ]]; then
-        printf '\n%s (%s)\n\n' \
-            "locktest: test failed!" \
-            "you need to commit both package-lock.json and package.json" >&2
+    if [[  "$no_meta" == "true" && "$no_lock" == "false" ]]; then
+        echo >&2;
+        echo "locktest: test failed!" >&2;
+        printf '    %s\n' \
+            "It seems that you've tried to commit a lock file without any changes"\
+            "done in 'package-lock.json'! This operation will be blocked as lockfile"\
+            "should not be bumped unless a development tree has changed in some way"\
+            "or commit is made that bumps package version and the new tag is going"\
+            "to be released" >&2
+        echo >&2;
         return 1;
     fi
 }
