ac32ff522bdd99c4f215fff6e8236d11d93d343b
SpacingBat3 <spacingbat3@gmail.com>
2025-11-17 23:44:54 +0100

    Remove unnecesary APIs in preload.
    Those are never going to be used for anything.

--- sources/code/renderer/modules/api.ts
+++ sources/code/renderer/modules/api.ts
@@ -36,54 +36,6 @@ export function generateSafeKey() {
 }
 
 /**
- * Gets list of the elements with `tagName` tag name that has any class assigned
- * which its name includes the `searchString`. This tries to replicate the
- * similar behaviour as the one achieved by the `.getElementsByClassName`
- * method, except it can allow for part of the class names as an input.
- *
- * This can be useful when trying to tweak the elements whose class names
- * includes some part being randomly generated for each build/version.
- *
- * **Currently unused in the code itself, reserved for WebCord's future API for
- * the extensions.**
- *//*
-export function findClass<S extends string>(searchString: S, tagName: keyof HTMLElementTagNameMap) {
-  const searchResult = new Set<`${string}${S}${string}`>();
-  for (const container of document.getElementsByTagName(tagName))
-    for (const classString of container.classList)
-      if(classString.includes(searchString))
-        searchResult.add(classString as `${string}${S}${string}`);
-  return [...searchResult];
-}/**/
-
-/**
- * A function that allows the access to the Discord API without the need of
- * sharing the user token to the untrusted scripts. Currently at
- * **Work-in-Progress** state.
- *
- * Currently unused in the code itself, reserved for WebCord's future API for
- * the extensions.**
- *
- * @param method
- * @param apiVersion
- * @param endpoint
- *//*
-export function sendRequest(method:"POST"|"GET", apiVersion: 6|7|8|9|10, endpoint:string, body?: XMLHttpRequestBodyInit) {
-  const request = new XMLHttpRequest();
-  request.open(method,new URL(document.URL).origin+"/api/v"+apiVersion.toString()+endpoint);
-  window.addEventListener("load", () => {
-    const token = window.localStorage.getItem("token");
-    if(token !== null) {
-      request.setRequestHeader("Authorization", token);
-      request.send(body);
-      return;
-    }
-    request.abort();
-    return;
-  });
-}/**/
-
-/**
  * Allows to navigate to the given path without reloading the entire Discord
  * page. In WebCord, this is used to handle `DEEP_LINK` requests.
  */
